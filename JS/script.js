2d0e60e0b20208dd8cc81a838824d11ef0fa1eda
Igor Santos <127339285+igorsantossdev@users.noreply.github.com>
2024-11-22 07:52:32 -0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const menu = document.querySelector('.menu');
+
+    menuToggle.addEventListener('click', () => {
+        menu.classList.toggle('show');
+    });
+});
